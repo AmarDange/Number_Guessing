79a80927a63a821ff165e200ca74d6257c634d6b
AmarDange <amardange218@gmail.com>
2023-03-16 21:02:45 +0000

    Add import pyfiglet

--- run.py
+++ run.py
@@ -2,6 +2,18 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
+import pyfiglet
+from termcolor import colored
+
+
+def Welcome():
+    """
+    Function to display home page
+    """
+    title = "Welcome  to  NUMBER  GUESSING  GAME "
+    print(colored(pyfiglet.figlet_format(title), "green"))
+Welcome()
+
 
 def guess_number():
     random_number = random.randint(1, 20)
@@ -11,25 +23,26 @@ def guess_number():
 
     while confirm_play.lower() == "yes":
 
-        guess = int(input("guess any number between 1 to 20:"))
+        guess = int(input("Guess any number between 1 to 50 : "))
 
-        if guess < 1 or guess > 20:
+        if guess < 1 or guess > 50:
             print("Please guess any number in the Range.")
 
         elif guess == random_number:
             print("Congratulations ", player_name, 'You Win !!!')
             attempts +=1
-            print("It took you ", attempts, "attempts to win this Game")
+            print("It took you ", attempts, "attempts to win this Game.")
             break
         elif guess > random_number:
-            print("Hint: Please try with smaller number")
+            print("Hint: Please try with smaller number.")
             attempts +=1
 
         elif guess < random_number:
-            print("Hint: Please try with larger number")
+            print("Hint: Please try with larger number.")
             attempts +=1
 
     else:
         print("Thanks ", player_name, "for your Time.")
 
 guess_number()
+
